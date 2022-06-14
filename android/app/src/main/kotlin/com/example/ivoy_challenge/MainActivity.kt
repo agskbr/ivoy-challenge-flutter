3507887fe7cf441f15cd2d2347dbc57530c969b0
Agustín Cabrera <69356711+agskbr@users.noreply.github.com>
2022-06-14 14:57:17 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/ivoy_challenge/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ivoy_challenge
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Ivoy challenge app',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Ivoy challenge"),
+      ),
+      body: const Center(
+        child: Text('Hello world'),
+      ),
+    );
+  }
+}
