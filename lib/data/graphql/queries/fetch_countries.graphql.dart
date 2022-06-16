b92fe6e6b48b888c97b6843c79ca9b3790b89093
Agustín Cabrera <69356711+agskbr@users.noreply.github.com>
2022-06-16 08:15:21 -0300

    Models to queries of countries

--- lib/data/graphql/queries/fetch_countries.graphql.dart
+++ lib/data/graphql/queries/fetch_countries.graphql.dart
@@ -8,6 +8,40 @@ import 'package:gql/ast.dart';
 part 'fetch_countries.graphql.g.dart';
 
 @JsonSerializable(explicitToJson: true)
+class Continent extends JsonSerializable with EquatableMixin {
+  Continent();
+
+  factory Continent.fromJson(Map<String, dynamic> json) =>
+      _$ContinentFromJson(json);
+
+  late String name;
+
+  late String code;
+
+  @override
+  List<Object?> get props => [name, code];
+  @override
+  Map<String, dynamic> toJson() => _$ContinentToJson(this);
+}
+
+@JsonSerializable(explicitToJson: true)
+class Language extends JsonSerializable with EquatableMixin {
+  Language();
+
+  factory Language.fromJson(Map<String, dynamic> json) =>
+      _$LanguageFromJson(json);
+
+  String? name;
+
+  late String code;
+
+  @override
+  List<Object?> get props => [name, code];
+  @override
+  Map<String, dynamic> toJson() => _$LanguageToJson(this);
+}
+
+@JsonSerializable(explicitToJson: true)
 class Country extends JsonSerializable with EquatableMixin {
   Country();
 
@@ -20,8 +54,12 @@ class Country extends JsonSerializable with EquatableMixin {
 
   late String emoji;
 
+  late Continent continent;
+
+  late List<Language> languages;
+
   @override
-  List<Object?> get props => [name, capital, emoji];
+  List<Object?> get props => [name, capital, emoji, continent, languages];
   @override
   Map<String, dynamic> toJson() => _$CountryToJson(this);
 }
@@ -71,7 +109,45 @@ final GET_ALL_COUNTRIES_QUERY_DOCUMENT = DocumentNode(definitions: [
                   alias: null,
                   arguments: [],
                   directives: [],
-                  selectionSet: null)
+                  selectionSet: null),
+              FieldNode(
+                  name: NameNode(value: 'continent'),
+                  alias: null,
+                  arguments: [],
+                  directives: [],
+                  selectionSet: SelectionSetNode(selections: [
+                    FieldNode(
+                        name: NameNode(value: 'name'),
+                        alias: null,
+                        arguments: [],
+                        directives: [],
+                        selectionSet: null),
+                    FieldNode(
+                        name: NameNode(value: 'code'),
+                        alias: null,
+                        arguments: [],
+                        directives: [],
+                        selectionSet: null)
+                  ])),
+              FieldNode(
+                  name: NameNode(value: 'languages'),
+                  alias: null,
+                  arguments: [],
+                  directives: [],
+                  selectionSet: SelectionSetNode(selections: [
+                    FieldNode(
+                        name: NameNode(value: 'name'),
+                        alias: null,
+                        arguments: [],
+                        directives: [],
+                        selectionSet: null),
+                    FieldNode(
+                        name: NameNode(value: 'code'),
+                        alias: null,
+                        arguments: [],
+                        directives: [],
+                        selectionSet: null)
+                  ]))
             ]))
       ]))
 ]);
--- lib/data/graphql/queries/fetch_countries.graphql.g.dart
+++ lib/data/graphql/queries/fetch_countries.graphql.g.dart
@@ -7,17 +7,45 @@ part of 'fetch_countries.graphql.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
+Continent _$ContinentFromJson(Map<String, dynamic> json) {
+  return Continent()
+    ..name = json['name'] as String
+    ..code = json['code'] as String;
+}
+
+Map<String, dynamic> _$ContinentToJson(Continent instance) => <String, dynamic>{
+      'name': instance.name,
+      'code': instance.code,
+    };
+
+Language _$LanguageFromJson(Map<String, dynamic> json) {
+  return Language()
+    ..name = json['name'] as String?
+    ..code = json['code'] as String;
+}
+
+Map<String, dynamic> _$LanguageToJson(Language instance) => <String, dynamic>{
+      'name': instance.name,
+      'code': instance.code,
+    };
+
 Country _$CountryFromJson(Map<String, dynamic> json) {
   return Country()
     ..name = json['name'] as String
     ..capital = json['capital'] as String?
-    ..emoji = json['emoji'] as String;
+    ..emoji = json['emoji'] as String
+    ..continent = Continent.fromJson(json['continent'] as Map<String, dynamic>)
+    ..languages = (json['languages'] as List<dynamic>)
+        .map((e) => Language.fromJson(e as Map<String, dynamic>))
+        .toList();
 }
 
 Map<String, dynamic> _$CountryToJson(Country instance) => <String, dynamic>{
       'name': instance.name,
       'capital': instance.capital,
       'emoji': instance.emoji,
+      'continent': instance.continent.toJson(),
+      'languages': instance.languages.map((e) => e.toJson()).toList(),
     };
 
 GetAllCountries$Query _$GetAllCountries$QueryFromJson(
